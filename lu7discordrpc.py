e6ec34254941793ee879cdf01e3b2641afe019a0
LuckUnstoppable7 <54641441+LuckVintage@users.noreply.github.com>
2024-02-01 18:44:16 +0000

    Update lu7discordrpc.py

--- lu7discordrpc.py
+++ lu7discordrpc.py
@@ -1,4 +1,13 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QFormLayout, QMessageBox
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QVBoxLayout,
+    QFormLayout,
+    QMessageBox,
+)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPalette, QColor, QIcon
 import rpc
@@ -6,37 +15,109 @@ import time
 from time import mktime
 import os
 
+
 class DiscordRPCApp(QWidget):
     def __init__(self):
         super().__init__()
 
         self.client_id_label = QLabel("Client ID:")
         self.client_id_entry = QLineEdit()
+        self.client_id_label.setToolTip(
+            "You can find this as the 'Application ID' in the Discord Developer Portal"
+        )
+        self.client_id_entry.setToolTip(
+            "You can find this as the 'Application ID' in the Discord Developer Portal"
+        )
 
         self.state_label = QLabel("State:")
         self.state_entry = QLineEdit()
+        self.state_label.setToolTip("Enter any text you like here")
+        self.state_entry.setToolTip("Enter any text you like here")
 
         self.small_text_label = QLabel("Small Text:")
         self.small_text_entry = QLineEdit()
+        self.small_text_label.setToolTip(
+            "This is the text that shows up when you hover your mouse over the small image"
+        )
+        self.small_text_entry.setToolTip(
+            "This is the text that shows up when you hover your mouse over the small image"
+        )
 
         self.small_image_label = QLabel("Small Image:")
         self.small_image_entry = QLineEdit()
+        self.small_image_label.setToolTip(
+            "This is the name of the image you would like. It must match exactly one of the images uploaded in the Discord Developer Portal."
+        )
+        self.small_image_entry.setToolTip(
+            "This is the name of the image you would like. It must match exactly one of the images uploaded in the Discord Developer Portal."
+        )
 
         self.large_text_label = QLabel("Large Text:")
         self.large_text_entry = QLineEdit()
+        self.large_text_label.setToolTip(
+            "This is the text that shows up when you hover your mouse over the large image"
+        )
+        self.large_text_entry.setToolTip(
+            "This is the text that shows up when you hover your mouse over the large image"
+        )
 
         self.large_image_label = QLabel("Large Image:")
         self.large_image_entry = QLineEdit()
+        self.large_image_label.setToolTip(
+            "This is the name of the image you would like. It must match exactly one of the images uploaded in the Discord Developer Portal."
+        )
+        self.large_image_entry.setToolTip(
+            "This is the name of the image you would like. It must match exactly one of the images uploaded in the Discord Developer Portal."
+        )
 
         self.toggle_button = QPushButton("Start RPC")
+        self.toggle_button.setObjectName(
+            "toggleButton"
+        )  # Set an object name for the toggle button
         self.toggle_button.clicked.connect(self.toggle_rpc)
         self.toggle_button.setStyleSheet("background-color: green; color: white;")
 
         self.current_status_label = QLabel("Current Status: Stopped")
         self.current_status_label.setStyleSheet("color: red;")
 
-        self.version_label = QLabel("v1.0.2 - 31 Jan 24")
+        self.version_label = QLabel("v1.0.3-alpha - 01 Feb 24")
+        self.prodwarning_label = QLabel("WARNING: NOT PRODUCTION READY!")
         self.version_label.setAlignment(Qt.AlignCenter)
+        self.prodwarning_label.setAlignment(Qt.AlignCenter)
+
+        # Set minimum height for QLabel widgets
+        self.client_id_label.setMinimumHeight(40)
+        self.state_label.setMinimumHeight(40)
+        self.small_text_label.setMinimumHeight(40)
+        self.small_image_label.setMinimumHeight(40)
+        self.large_text_label.setMinimumHeight(40)
+        self.large_image_label.setMinimumHeight(40)
+        self.prodwarning_label.setFixedSize(260, 20)
+
+        # set fixed height for QLineEdit
+        self.client_id_entry.setFixedSize(140, 25)
+        self.state_entry.setFixedSize(140, 25)
+        self.small_text_entry.setFixedSize(140, 25)
+        self.small_image_entry.setFixedSize(140, 25)
+        self.large_text_entry.setFixedSize(140, 25)
+        self.large_image_entry.setFixedSize(140, 25)
+
+
+        # Set alignment to the top
+        self.client_id_label.setAlignment(Qt.AlignTop)
+        self.state_label.setAlignment(Qt.AlignTop)
+        self.small_text_label.setAlignment(Qt.AlignTop)
+        self.small_image_label.setAlignment(Qt.AlignTop)
+        self.large_text_label.setAlignment(Qt.AlignTop)
+        self.large_image_label.setAlignment(Qt.AlignTop)
+
+        self.client_id_entry.setToolTipDuration(5000)  # Set tooltip duration in milliseconds
+        self.state_entry.setToolTipDuration(5000)
+        self.small_text_entry.setToolTipDuration(5000)
+        self.small_image_entry.setToolTipDuration(5000)
+        self.large_text_entry.setToolTipDuration(5000)
+        self.large_image_entry.setToolTipDuration(5000)
+
 
         layout = QFormLayout()
         layout.addRow(self.client_id_label, self.client_id_entry)
@@ -48,6 +129,7 @@ class DiscordRPCApp(QWidget):
         layout.addRow(self.toggle_button)
         layout.addRow(self.current_status_label)
         layout.addRow(self.version_label)
+        layout.addRow(self.prodwarning_label)
 
         self.setLayout(layout)
 
@@ -58,7 +140,9 @@ class DiscordRPCApp(QWidget):
         self.setFixedSize(self.sizeHint())
 
         # Set application icon
-        icon_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "logo.icns")
+        icon_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "logo.icns"
+        )
         self.setWindowIcon(QIcon(icon_path))
 
         # Set background and foreground colors
@@ -75,10 +159,12 @@ QWidget {
     font-family: Arial, sans-serif;
 }
 
-QPushButton {
-    padding: 0px 20px;
+
+QPushButton#toggleButton {
+    padding: 13px 20px;
     border: none;
     border-radius: 4px;
+    min-height: 13px; /* Set the minimum height for the button */
 }
 
 QLineEdit {
@@ -91,7 +177,7 @@ QLabel {
     font-size: 14px;
     margin-bottom: 8px;
 }
-        """
+"""
 
         self.setStyleSheet(stylesheet)
 
@@ -116,7 +202,11 @@ QLabel {
             self.toggle_button.setText("Stop RPC")
             QMessageBox.information(self, "Success", "RPC started successfully.")
         except Exception as e:
-            QMessageBox.critical(self, "Error", f"Error starting RPC: {str(e)}. Make sure Discord is currently running!")
+            QMessageBox.critical(
+                self,
+                "Error",
+                f"Error starting RPC: {str(e)}. Make sure Discord is currently running!",
+            )
 
     def stop_rpc(self):
         try:
@@ -124,9 +214,15 @@ QLabel {
                 self.rpc_obj.close()
                 self.rpc_obj = None  # Set to None after stopping
                 self.update_status_label()
-                self.toggle_button.setStyleSheet("background-color: green; color: white;")
+                self.toggle_button.setStyleSheet(
+                    "background-color: green; color: white;"
+                )
                 self.toggle_button.setText("Start RPC")
-                QMessageBox.information(self, "Success", "RPC stopped successfully. It may take a few seconds for Discord to register the change.")
+                QMessageBox.information(
+                    self,
+                    "Success",
+                    "RPC stopped successfully. It may take a few seconds for Discord to register the change.",
+                )
             else:
                 QMessageBox.warning(self, "Warning", "No active RPC to stop.")
         except Exception as e:
@@ -199,7 +295,8 @@ QLabel {
         self.save_data()
         event.accept()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = QApplication([])
     window = DiscordRPCApp()
     window.setWindowTitle("LU7 Discord RPC")
